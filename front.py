cad09958282111e7ec7d120620759f7f4c517d70
nurkul987-lgtm <nurkul987@gmail.com>
2026-05-14 20:43:17 +0600

    fix model loading

--- front.py
+++ front.py
@@ -1,41 +1,49 @@
 import streamlit as st
 import torch
+import torch.nn as nn
 from PIL import Image
 from torchvision import transforms
 
 st.title('Fashion MNIST Predictor')
 
-model = torch.load('model_fashion_nur.pth', map_location='cpu')
+# ---- MODEL CLASS (бул маанилүү!) ----
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+# ---- LOAD MODEL ----
+model = Net()
+model.load_state_dict(torch.load('model_fashion_nur.pth', map_location='cpu'))
 model.eval()
 
+# ---- CLASSES ----
 classes = [
-    'T-shirt/top',
-    'Trouser',
-    'Pullover',
-    'Dress',
-    'Coat',
-    'Sandal',
-    'Shirt',
-    'Sneaker',
-    'Bag',
-    'Ankle boot'
+    'T-shirt/top','Trouser','Pullover','Dress','Coat',
+    'Sandal','Shirt','Sneaker','Bag','Ankle boot'
 ]
 
+# ---- TRANSFORM ----
 transform = transforms.Compose([
     transforms.Grayscale(),
     transforms.Resize((28, 28)),
     transforms.ToTensor()
 ])
 
-uploaded_file = st.file_uploader(
-    'загрузи фото',
-    type=['png', 'jpg', 'jpeg']
-)
+# ---- UI ----
+uploaded_file = st.file_uploader('загрузи фото', type=['png','jpg','jpeg'])
 
 if uploaded_file is not None:
 
     image = Image.open(uploaded_file)
-
     st.image(image, width=250)
 
     if st.button('Predict'):
