4437402f61d3c28a6518416bb8c7a57165855bcb
nurkul987-lgtm <nurkul987@gmail.com>
2026-05-14 20:47:38 +0600

    final fix working model

--- front.py
+++ front.py
@@ -1,24 +1,53 @@
 import streamlit as st
 import torch
+import torch.nn as nn
 from PIL import Image
 from torchvision import transforms
 
 st.title('Fashion MNIST Predictor')
 
-model = torch.load('model_fashion_nur.pth', map_location='cpu')
+# ---------------- MODEL ----------------
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(28 * 28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 10)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+# ---------------- LOAD MODEL ----------------
+path = 'model_fashion_nur.pth'
+
+checkpoint = torch.load(path, map_location='cpu')
+
+# эгер state_dict болсо
+if isinstance(checkpoint, dict):
+    model = Net()
+    model.load_state_dict(checkpoint)
+else:
+    model = checkpoint
+
 model.eval()
 
+# ---------------- CLASSES ----------------
 classes = [
     'T-shirt/top','Trouser','Pullover','Dress','Coat',
     'Sandal','Shirt','Sneaker','Bag','Ankle boot'
 ]
 
+# ---------------- TRANSFORM ----------------
 transform = transforms.Compose([
     transforms.Grayscale(),
     transforms.Resize((28, 28)),
     transforms.ToTensor()
 ])
 
+# ---------------- UI ----------------
 uploaded_file = st.file_uploader('загрузи фото', type=['png','jpg','jpeg'])
 
 if uploaded_file:
